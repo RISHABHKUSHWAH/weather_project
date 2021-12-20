2ecc09d6518791751c27690c7a90f2e4ae6e2eaa
rishabhkushwah <rishabhkushwah>
2021-12-20 09:54:30 +0530

    first commit for push code to github

---
+++ check_weather/weatherApp/views.py
@@ -0,0 +1,31 @@
+import urllib.request
+import json
+from django.shortcuts import render
+
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=95578d40efe3de97b42506a76da8dec4').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+            
+        }
+        return render(request,"result/result.html",data)
+    else:
+        data = {}
+
+    return render(request, "main1/index.html")
